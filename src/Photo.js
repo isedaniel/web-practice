c762728f5783aca68b51e00d5cae99ad5428bb4b
Daniel Ise <ise.daniel@gmail.com>
2022-10-21 17:14:21 -0300

    initial project

---
+++ src/Photo.js
@@ -0,0 +1,9 @@
+import profilePicture from './images/profile-pic.jpg'
+
+export default function() {
+  return (
+    <div className="image-div">
+      <img src={ profilePicture } />
+    </div>
+  )
+}
