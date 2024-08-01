abccfddd1b4c2b08fece2fefec89424dd7b3d79d
Daniel <ise.daniel@gmail.com>
2024-08-01 11:12:53 -0300

    restarting repo

--- src/Photo.js
+++
@@ -1,9 +0,0 @@
-import profilePicture from './images/profile-pic.jpg'
-
-export default function() {
-  return (
-    <div className="photo-div">
-      <img src={ profilePicture } />
-    </div>
-  )
-}
